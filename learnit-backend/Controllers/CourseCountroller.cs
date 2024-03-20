536d5097a3722602fc56a7e030b03b701daa5fad
sudhz <shudhanshumakwana@gmail.com>
2024-03-20 15:06:29 +0530

    controller for frequently bought courses added

--- learnit-backend/Controllers/CourseCountroller.cs
+++ learnit-backend/Controllers/CourseCountroller.cs
@@ -2,8 +2,6 @@ using learnit_backend.Data;
 using learnit_backend.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text.Json;
 
 namespace learnit_backend.Controllers;
 
@@ -16,7 +14,6 @@ public class CourseController(LearnitDbContext context) : ControllerBase
     [HttpGet()]
     public async Task<ActionResult<IEnumerable<Course>>> GetCourses()
     {
-        
         var courses = await _context.Courses.ToListAsync();
 
         if (courses == null || !courses.Any())
@@ -25,73 +22,6 @@ public class CourseController(LearnitDbContext context) : ControllerBase
         }
 
         return Ok(courses);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<Course>> CreateCourse(Course course)
-    {
-        _context.Courses.Add(course);
-        await _context.SaveChangesAsync();
-        return CreatedAtAction(nameof(Course), new { id = course.CourseId }, course);
-    }
-
-    [HttpPut("{id}")]
-    public async Task<IActionResult> UpdateCourse(int id, Course course)
-    {
-        if (id != course.CourseId)
-        {
-            return BadRequest();
-        }
-
-        _context.Entry(course).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
-        Console.WriteLine(course.CourseName.ToString());
-        return NoContent();
-    }
-
-    [HttpGet("{id}")]
-    public async Task<ActionResult<Course>> GetCourse(int id)
-    {
-        var course = await _context.Courses.FindAsync(id);
-
-        if (course == null)
-        {
-            return NotFound();
-        }
-
-        return course;
-    }
-
-    [HttpDelete("{id}")]
-    public async Task<IActionResult> DeleteCourse(int id)
-    {
-        var course = await _context.Courses.FindAsync(id);
-        if (course == null)
-        {
-            return NotFound();
-        }
-
-        _context.Courses.Remove(course);
-        await _context.SaveChangesAsync();
-
-        return NoContent();
-    }
-
-    [HttpPut("{id}/description")]
-    public async Task<IActionResult> UpdateCourseDescription(int id, [FromBody] JsonElement payload)
-    {
-        var course = await _context.Courses.FindAsync(id);
-        if (course == null)
-        {
-            return NotFound();
-        }
-
-        string UDescription = payload.GetProperty("UDescription").ToString();
-        course.CourseDescription = UDescription;
-        _context.Entry(course).State = EntityState.Modified;
-        await _context.SaveChangesAsync();
-
-        return NoContent(); // Return a 204 No Content status code to indicate success
     }
 
     [HttpGet("{courseId}/top-courses")]
@@ -124,9 +54,4 @@ public class CourseController(LearnitDbContext context) : ControllerBase
         }
     }
 
-}
-private bool CourseExists(int id)
-    {
-        return _context.Courses.Any(e => e.CourseId == id);
-    }
 }
