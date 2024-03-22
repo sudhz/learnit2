4677d827b9a37a9191d5fa9e3784902612872e8d
sudhz <shudhanshumakwana@gmail.com>
2024-03-22 10:10:48 +0530

    updated student home

--- learnit-frontend/src/components/student/StudentHome.tsx
+++ learnit-frontend/src/components/student/StudentHome.tsx
@@ -1,50 +1,108 @@
-import { Box, Typography, Button, Container } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+} from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { GetCoursesByStudentId } from "../../services/api/courseService";
+import Course from "../../model/course";
+import useLocalStorage from "../../services/hooks/useLocalStorage";
+import CourseCard from "../CourseCard";
 
 const StudentHome = () => {
   const navigate = useNavigate();
-  const backgroundStyle = {
-    backgroundImage: `url("https://asset.gecdesigns.com/img/background-templates/isometric-e-learning-background-template-1612282245987-cover.webp")`, // Replace with your actual image URL
-    backgroundSize: "actual",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    minHeight: "60vh",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-  };
-
-  const contentStyle = {
-    margin: "20px",
-    padding: "20px",
-    border: "1px solid grey",
-    borderRadius: "2px",
-    backgroundColor: "rgba(255, 255, 255, 0.8)",
-    textAlign: "center",
-  };
-
+  const [courses, setCourses] = useState<Course[]>([]);
+  const { getItem } = useLocalStorage("user");
+  const id = getItem().id;
+  useEffect(() => {
+    const fetchCoursesByStudentId = async () => {
+      try {
+        const data = await GetCoursesByStudentId(id);
+        setCourses(data);
+      } catch (error) {
+        console.error(`Error fetching courses: ${error}`);
+      }
+    };
+    fetchCoursesByStudentId();
+  }, [id]);
   return (
-    <Box sx={backgroundStyle}>
-      <Container maxWidth="sm">
-        <Box sx={contentStyle}>
+    <Container maxWidth="lg" sx={{ minHeight: "100vh" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
           <Typography variant="h4" gutterBottom>
-            Welcome
+            Welcome to Learnit!
           </Typography>
           <Typography variant="body1" gutterBottom>
-            Here you can find all the resources that need to update yourself
+            Welcome to the future of learning, where convenience meets
+            innovation. Our platform offers a wide range of online courses
+            designed to enhance your skills, expand your knowledge, and open up
+            new opportunities. Whether you're looking to advance in your career,
+            explore new hobbies, or simply learn something new, we've got you
+            covered.
           </Typography>
+          {/* Resume Studying Button */}
           <Button
             variant="contained"
             color="primary"
             size="large"
+            sx={{ marginTop: "20px" }}
+            onClick={() => navigate("/courses")}
+          >
+            Browse all courses
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img
+            src="https://logodix.com/logo/2149417.png"
+            alt="Welcome Image"
+            style={{ width: "60%", height: "auto" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="h5" gutterBottom>
+            Featured Courses
+          </Typography>
+          {courses ? (
+            <Grid container spacing={2}>
+              {courses.slice(0, 3).map((course, idx) => (
+                <Grid item key={idx} md={4} xs={8} sm={8} alignItems="center">
+                  <CourseCard
+                    id={course.courseId}
+                    linkTo={`/student/course/${course.courseId}`}
+                    title={course.courseName}
+                    description={course.courseDescription}
+                    imgUrl={course.imgUrl}
+                    includePrice={false}
+                    price={course.price}
+                    createdAt={new Date(course.createdAt)}
+                    showProgress={true}
+                    progress={course.progress}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            "Loading the courses..."
+          )}
+          <Button
+            variant="outlined"
+            color="primary"
+            size="large"
+            style={{ marginTop: "20px" }}
             onClick={() => navigate("/student/courses")}
           >
-            Start Learning
+            Show More
           </Button>
-        </Box>
-      </Container>
-    </Box>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
