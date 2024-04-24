42c951a35bf4fecbd43b83309988e1c543794b72
P12B582 <anushamranusha407@gmail.com>
2024-04-24 10:42:34 +0530

    changes in courseLanding

--- learnit-frontend/src/components/CourseLanding.tsx
+++ learnit-frontend/src/components/CourseLanding.tsx
@@ -155,6 +155,7 @@ const CourseLandingPage: React.FC = () => {
           sx={{ color: "#3498db", fontWeight: "bold" }}
         >
           Course Descripton
+          Course Desciption
         </Typography>
         <Card
           sx={{
